9e43d9aaff0b3306207530191b7e8e86aa2d7a68
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-05-15 12:35:00 +0900

    🚑 Fix : server > service > readArticlesPathList()  & readDownloadArticles()버그 픽스
    게시물 경로 오류 불러와지지않는 버그 픽스

--- project/src/app/api/(etc)/(service)/bucket/articles.ts
+++ project/src/app/api/(etc)/(service)/bucket/articles.ts
@@ -2,67 +2,69 @@ import { supabase } from "@/app/api/supabaseServer";
 import { supabase as supabaseClient } from "@/lib/supabaseClient";
 import { decodeFromBase64, encodeToBase64 } from "@/utils/base64";
 
-////////// READ : 파일 목록 가져오기
 export type ArticlePathListType = {
   articlePath: string;
-  imagePathList: string[];
-  htmlPathList: string;
+  imagePathList: string[] | [];
+  htmlPathList: string | null;
 };
 
-export async function readArticlesPathList(path: string) {
+////////// READ : 파일 목록 가져오기
+export async function readArticlesPathList(wpId: string) {
   try {
     // 경로를 매개변수로 받아 유연하게 사용
-    const { data: articlePathListData } = await supabaseClient.storage.from("articles").list(path);
+    const { data: articlePathListResponseData } = await supabaseClient.storage.from("articles").list(wpId);
 
     // 예외처리 : 경로 데이터가 없거나 빈 배열이라면 빈 배열 반환
-    if (!articlePathListData || articlePathListData.length === 0) {
+    if (!articlePathListResponseData || articlePathListResponseData.length === 0) {
       return [];
     }
 
+    const articlesPathList = articlePathListResponseData.map((el) => el.name);
+
     // Promise.all을 사용하여 모든 비동기 작업이 완료될 때까지 기다림
-    const articlesPathList = await Promise.all(
+    const result = await Promise.all(
+      articlesPathList.map(async (articlePath) => {
+        const imageFolderPath = `${wpId}/${articlePath}/img`;
+        const htmlFilePath = `${wpId}/${articlePath}`;
 
-      articlePathListData.map(async (item) => {
-        const imageFolderPath = `${path}/${item.name}/img`; // 'image'가 아닌 'img'로 수정
-        const htmlFilePath = `${path}/${item.name}`;
-
-        const { data: imagePathListResponse } = await supabase.storage.from("articles").list(imageFolderPath);
-        const { data: htmlPathResponse } = await supabase.storage.from("articles").list(htmlFilePath);
+        const { data: imagePathListResponseData } = await supabase.storage.from("articles").list(imageFolderPath);
+        const { data: htmlPathListResponseData } = await supabase.storage.from("articles").list(htmlFilePath);
 
         // 이미지 파일 경로 목록
         let imagePathList: string[] = [];
 
         // 예외처리 : 이미지 파일 경로 목록이 없거나 빈 배열이라면 빈 배열 반환
-        if (imagePathListResponse && imagePathListResponse?.length > 0) {
-          imagePathList = imagePathListResponse?.map((item) => {
+        if (!imagePathListResponseData || imagePathListResponseData?.length === 0) {
+          imagePathList = [];
+        } else {
+          imagePathList = imagePathListResponseData?.map((item) => {
             const decodedFileName = decodeFromBase64(item.name.split(".")[0]);
             const decodedExtension = item.name.split(".").pop();
 
             return `${decodedFileName}.${decodedExtension}`;
           });
-        } else {
-          imagePathList = [];
         }
 
         // HTML 파일 경로
         let htmlPathList;
 
         // 예외처리 : HTML 파일 경로가 없거나 빈 배열이라면 null 반환
-        if (htmlPathResponse && htmlPathResponse?.length > 0) {
-          htmlPathList = htmlPathResponse?.filter((item) => item.name.endsWith(".html"));
-        } else {
+        if (!htmlPathListResponseData || htmlPathListResponseData?.length === 0) {
           htmlPathList = null;
+        } else {
+          htmlPathList = htmlPathListResponseData?.filter((item) => item.name.endsWith(".html")).map((el) => el.name)[0];
         }
 
+        // 게시물 경로 반환
         return {
-          articlePath: item.name,
-          imagePathList: imagePathList,
-          htmlPathList: htmlPathList?.[0].name,
+          articlePath,
+          imagePathList,
+          htmlPathList,
         };
       })
     );
 
-    return articlesPathList;
+    return result;
   } catch (error) {
     return { data: null, error: error };
   }
@@ -76,18 +78,23 @@ export async function readDownloadArticles(wpId: string, articlePathList: Articl
       return [];
     }
 
+    // 게시물 파일 다운로드
     const articlesFiles = await Promise.all(
-
       articlePathList.map(async (article) => {
-        // 경로 분리
+        // 이미지, HTML 경로 분리
         const { imagePathList, htmlPathList } = article;
+
+        // 기본 경로
         const basePath = `${wpId}/${article.articlePath}`;
 
-        // 이미지 파일 다운로드
+        // 이미지 파일 리스트
         let imageFileList: Blob[] = [];
 
-        if (imagePathList.length > 0) {
-          imageFileList = await Promise.all(
+        // 예외처리 : 이미지 파일 경로 목록이 없거나 빈 배열이라면 빈 배열 반환
+        if (!Array.isArray(imagePathList) || imagePathList.length === 0) {
+          imageFileList = [];
+        } else {
+          imageFileList = (await Promise.all(
             imagePathList.map(async (path) => {
               const fileName = encodeToBase64(path.split(".")[0]);
               const extension = path.split(".").pop();
@@ -96,25 +103,23 @@ export async function readDownloadArticles(wpId: string, articlePathList: Articl
               const { data: imageFile } = await coreDownloadFile("articles", `${basePath}/img/${imagePath}`);
               return imageFile;
             })
-          ) as Blob[];
-        } else {
-          imageFileList = [];
+          )) as Blob[];
         }
 
-        // HTML 파일 다운로드
+        // HTML 파일
         let htmlFile;
 
-        if (htmlPathList) {
+        // 예외처리 : HTML 파일 경로가 없거나 빈 배열이라면 null 반환
+        if (!htmlPathList) {
+          htmlFile = null;
+        } else {
           const { data: downloadedHtmlFile } = await coreDownloadFile("articles", `${basePath}/${htmlPathList}`);
           htmlFile = downloadedHtmlFile;
-          return htmlFile;
-        } else {
-          htmlFile = null;
         }
 
         return {
-          imageFileList: imageFileList,
-          htmlFile: htmlFile,
+          imageFileList,
+          htmlFile,
         };
       })
     );
